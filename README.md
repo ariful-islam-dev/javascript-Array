70fdbae4e74790c9c9e549f355796cb68181a253
nodeAriful <ariful.node82@gmail.com>
2021-01-08 18:19:35 +0600

    thinking for reduce method

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 # Array 
 - How to using Array for problem solving
 
-01. arr.map()
-02. arr.filter()
-03. arr.find()
-04. arr.some()
-05. arr.every()
+## reduce() Method
--- lecture1.js
+++ lecture1.js
@@ -1,48 +1,14 @@
-let names = ["Ariful Islam", "Johirul Islam", "Azizul Islam", "Asia Akter"];
+const arr = [1, 2, 3, 4, 5];
+const red = arr.reduce((acc, curr) => acc + curr);
+console.log(red);
 
-for (let i = 0; i < names.length; i++) {
-  //   console.log(names[i]);
-}
-
-for (let i = 0; i < names.length; i++) {
-  //   console.log(`${i + 1}: ${names[i]}`);
-}
-
-//Iterationg -> Business  Logic
-
-// function reuse(arr, logic) {
-//   for (let i = 0; i < arr.length; i++) {
-//     logic(arr[i], i, arr);
-//   }
-// }
-// function log(value){
-//     console.log(value);
-// }
-// function logic(value, index, arr) {
-//   console.log(value, index, arr);
-// }
-// // reuse([])
-// reuse([1, 2], log);
-// reuse(["One", "Two", "Three"], logic);
-
-function myForeach(arr, cb) {
-  for (let i = 0; i < arr.length; i++) {
-    cb(arr[i], i, arr);
-  }
-}
-
-const nums = [1, 2, 3, 4];
-let sum = 0;
-
-myForeach(nums, function sumFunc(v) {
-  sum += v;
-});
-console.log(sum);
-
-myForeach(names, (value, index) => {
-  console.log(`${index + 1}: ${value}`);
-});
-
-names.forEach((value, index)=>{
-    console.log(`[Array] ${index + 1}: ${value}`);
-})
+const names = ["Ariful Islam", "Abdullah Al Zarif", "Aysha Mone"];
+const votes = [
+  "Java",
+  "Java",
+  "Python",
+  "Java",
+  "Javascript",
+  "Python",
+  "Javascript",
+];
